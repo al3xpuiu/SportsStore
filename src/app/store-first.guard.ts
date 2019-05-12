018ce71ab897419ee5bb00cc40b5f2227e395aea
al3xpuiu <puiualexandru87@yahoo.com>
2019-05-12 18:37:57 +0200

    added order

---
+++ src/app/store-first.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {StoreComponent} from './store/store.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreFirstGuard implements CanActivate {
+
+  private firstNavigation = true;
+
+  constructor(private router: Router) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.firstNavigation) {
+      this.firstNavigation = false;
+      if (next.component != StoreComponent) {
+        this.router.navigateByUrl('/');
+        return false;
+      }
+    }
+    return true;
+  }
+}
